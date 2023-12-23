317e2d7bb3f037b735c75fb7e614bbc514135f38
d68864767 <153361541+d68864767@users.noreply.github.com>
2023-12-22 19:57:47 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+// script.js
+
+// This script file is currently empty as there are no specific JavaScript functionalities mentioned in the project description.
+// However, you can add interactivity to your website using JavaScript. For example, you can create a responsive navigation menu, 
+// add animations, handle form submissions, etc. 
+
+// Here is a simple example of how you can use JavaScript to display a welcome message to the user:
+
+window.onload = function() {
+    alert("Welcome to AI in Business: A Comprehensive Guide to Boosting Productivity and Profitability!");
+}
